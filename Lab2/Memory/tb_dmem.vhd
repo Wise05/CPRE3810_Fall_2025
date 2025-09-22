ef26644aa54e9378f7c064aa0a1b9a9f1b181c71
Zevan Gustafson <zevangus@gmail.com>
2025-09-22 12:42:24 -0500

    routine commit

--- Lab2/Memory/tb_dmem.vhd
+++ Lab2/Memory/tb_dmem.vhd
@@ -8,10 +8,7 @@ end entity;
 
 architecture sim of tb_dmem is
     constant DATA_WIDTH : natural := 32;
-    constant ADDR_WIDTH : natural := 10;
-
-    constant gCLK_HPER : time := 5 ns; 
-    constant cCLK_PER  : time := gCLK_HPER * 2;
+    constant ADDR_WIDTH : natural := 10;  
 
     signal clk   : std_logic := '0';
     signal addr  : std_logic_vector(ADDR_WIDTH-1 downto 0) := (others => '0');
@@ -19,22 +16,20 @@ architecture sim of tb_dmem is
     signal we    : std_logic := '0';
     signal q     : std_logic_vector(DATA_WIDTH-1 downto 0);
 
-    -- storage for read values
+    
     type mem_array_t is array (0 to 9) of std_logic_vector(DATA_WIDTH-1 downto 0);
     signal temp_values : mem_array_t;
 begin
 
     clk_process : process
     begin
-        while true loop
-            clk <= '0';
-            wait for gCLK_HPER;
-            clk <= '1';
-            wait for gCLK_HPER;
-        end loop;
+        clk <= '0';
+        wait for 5 ns;
+        clk <= '1';
+        wait for 5 ns;
     end process;
 
-    -- Instantiate the data memory 
+    -- Instantiate the data memory
     dmem : entity work.mem
         generic map (
             DATA_WIDTH => DATA_WIDTH,
@@ -56,28 +51,34 @@ begin
         -- read first 10 values
         for i in 0 to 9 loop
             addr <= std_logic_vector(to_unsigned(i, ADDR_WIDTH));
-            wait for gCLK_HPER/2; 
-            temp_values(i) <= q;
-            wait for cCLK_PER;
+            wait for 2 ns; 
+            temp_values(i) <= q; -- store in testbench signal
+            report "Read value at addr " & integer'image(i) &
+                   " = " & to_hstring(q);
+            wait for 8 ns;
         end loop;
 
-        -- write values to addr starting at 0x100
+        -- (c) WRITE values to addr starting at 0x100
         for i in 0 to 9 loop
             addr <= std_logic_vector(to_unsigned(16#100# + i, ADDR_WIDTH));
             data <= temp_values(i);
             we   <= '1';
-            wait until rising_edge(clk);  -- write on clock
+            wait until rising_edge(clk);  -- write happens here
             we   <= '0';
-            wait for cCLK_PER;
+            report "Wrote value " & to_hstring(data) &
+                   " to addr " & integer'image(16#100# + i);
         end loop;
 
-        -- Verify by reading back
+        -- (Optional) Verify by reading back
         for i in 0 to 9 loop
             addr <= std_logic_vector(to_unsigned(16#100# + i, ADDR_WIDTH));
-            wait for gCLK_HPER/2;
-            wait for cCLK_PER;
+            wait for 2 ns;
+            report "Verify addr " & integer'image(16#100# + i) &
+                   " = " & to_hstring(q);
+            wait for 8 ns;
         end loop;
 
+        report "Testbench finished." severity note;
         wait;
     end process;
 end architecture;
