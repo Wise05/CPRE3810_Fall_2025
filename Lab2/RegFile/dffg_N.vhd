20ea9fb49f7d1cfee1544722b80b0740f9fe9b75
Zevan Gustafson <zevangus@gmail.com>
2025-09-27 10:11:03 -0500

    what is wrong IDK

--- Lab2/RegFile/dffg_N.vhd
+++ Lab2/RegFile/dffg_N.vhd
@@ -6,36 +6,36 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 
 entity dffg_N is 
-    generic(N : integer := 32);
-    port(i_CLK        : in std_logic;
-       i_RST        : in std_logic;     -- Reset input
-       i_WE         : in std_logic;     -- Write enable input
-       i_D          : in std_logic_vector(N-1 downto 0);     -- Data value input
-       o_Q          : out std_logic_vector(N-1 downto 0)); -- Data value output
-end dffg_N;
+  generic(N : integer := 32);
+  port(
+    i_CLK : in  std_logic;
+    i_RST : in  std_logic;
+    i_WE : in  std_logic;
+    i_D : in  std_logic_vector(N-1 downto 0);
+    o_Q : out std_logic_vector(N-1 downto 0)
+  );
+end entity;
 
 architecture structural of dffg_N is
-
-    component dffg is 
-        port(i_CLK        : in std_logic;     -- Clock input
-        i_RST        : in std_logic;     -- Reset input
-        i_WE         : in std_logic;     -- Write enable input
-        i_D          : in std_logic;     -- Data value input
-        o_Q          : out std_logic);
-    end component;
-
+  component dffg
+    port(
+      i_CLK : in std_logic;
+      i_RST : in std_logic;
+      i_WE : in std_logic;
+      i_D : in std_logic;
+      o_Q : out std_logic
+    );
+  end component;
 begin
-
-    G_NBIT_REGISTER: for i in 0 to N-1 generate
-        REGI: dffg port map (
-            i_CLK => i_CLK,
-            i_RST => i_RST,
-            i_WE => i_WE,
-            i_D => i_D(i),
-            o_Q => o_Q(i)
-        );
-    end generate G_NBIT_REGISTER;
-
-end structural;
-
+  gen_regs : for i in 0 to N-1 generate
+    bit_ff: dffg
+      port map (
+        i_CLK => i_CLK,
+        i_RST => i_RST,
+        i_WE => i_WE,
+        i_D => i_D(i),
+        o_Q => o_Q(i)
+      );
+  end generate;
+end architecture;
 
--- Lab2/RegFile/tb_dffg_N.vhd
+++ Lab2/RegFile/tb_dffg_N.vhd
@@ -13,11 +13,11 @@ architecture behavior of tb_dffg is
 
   component dffg_N
     generic (N : integer := 32);
-    port(i_CLK        : in std_logic;     -- Clock input
-         i_RST        : in std_logic;     -- Reset input
-         i_WE         : in std_logic;     -- Write enable input
-         i_D          : in std_logic_vector(N-1 downto 0);     -- Data value input
-         o_Q          : out std_logic_vector(N-1 downto 0));   -- Data value output
+    port(i_CLK : in std_logic;
+         i_RST : in std_logic;
+         i_WE : in std_logic;
+         i_D : in std_logic_vector(N-1 downto 0);
+         o_Q : out std_logic_vector(N-1 downto 0));
   end component;
 
   signal s_CLK, s_RST, s_WE  : std_logic;
