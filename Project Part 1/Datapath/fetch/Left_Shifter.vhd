8434508d9846f260aa5b05a9a80294775aa90389
Zevan Gustafson <zevangus@gmail.com>
2025-10-08 15:02:02 -0500

    fetch stuff

--- Project Part 1/Datapath/fetch/Left_Shifter.vhd
+++ Project Part 1/Datapath/fetch/Left_Shifter.vhd
@@ -5,4 +5,11 @@ use work.regfile_pkg.all;
 entity Left_Shifter is 
   port (
     i : in std_logic_vector(31 downto 0);
-       )
+    o : out std_logic_vector(31 downto 0);
+  )
+end Left_Shifter;
+
+architecture structural or Left_Shifter is
+begin
+  o <= shift_left(i, 1);
+end structural
