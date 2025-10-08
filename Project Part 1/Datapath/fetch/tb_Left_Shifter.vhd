76515f6e2a95ac4d27f5f36faab842a0e2e77006
Zevan Gustafson <zevangus@gmail.com>
2025-10-08 15:13:00 -0500

    test bench for left shifter

---
+++ Project Part 1/Datapath/fetch/tb_Left_Shifter.vhd
@@ -0,0 +1,36 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity tb_Left_Shifter is 
+  end entity;
+
+architecture sim of tb_Left_Shifter is 
+  i : std_logic_vector(31 downto 0);
+  o : std_logic_vector(31 downto 0);
+
+  component Left_Shifter
+    port (
+i : in std_logic_vector(31 downto 0);
+      o : out std_logic_vector(31 downto 0);
+   );
+  end component
+
+begin 
+DUT: Left_Shifter 
+  port map (
+    i => i,
+    o => 0
+  );
+
+stim: process
+begin
+  i <= x"00000001";
+  wait for 10ns;
+
+  i <= x"800000000";
+  wait for 10ns;
+
+  wait;
+end process;
+end architecture sim;
