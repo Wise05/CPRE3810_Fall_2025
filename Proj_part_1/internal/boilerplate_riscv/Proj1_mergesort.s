9c0ada3f54f6012404e559ed2e91774492ef872a
Zevan Gustafson <zevangus@gmail.com>
2025-10-29 13:35:36 -0500

    Hopefully working assembly for the test applcations

---
+++ Proj_part_1/internal/boilerplate_riscv/Proj1_mergesort.s
@@ -0,0 +1,174 @@
+############################################################
+# merge_sort_test.s
+# Recursive Merge Sort implemented using only allowed RV32I
+# instructions. Works in RARS.
+############################################################
+
+.data
+array:  .word 9, 3, 7, 1, 8, 2, 6, 4
+n:      .word 8
+temp:   .space 32           # temporary buffer (8 words * 4B)
+
+newline: .asciiz "\n"
+
+.text
+.globl main
+
+############################################################
+# main: entry point
+############################################################
+main:
+    la   x10, array          # x10 = base address of array
+    lw   x11, n              # x11 = number of elements
+    addi x12, x0, 0          # left index = 0
+
+    # call mergesort(array, left=0, right=n-1)
+    addi x13, x11, -1
+    jal  x1, mergesort
+
+    # Program finished — halt
+    wfi
+
+############################################################
+# mergesort(base=x10, left=x12, right=x13)
+# if left < right:
+#   mid = (left + right) / 2
+#   mergesort(left, mid)
+#   mergesort(mid+1, right)
+#   merge(left, mid, right)
+############################################################
+mergesort:
+    addi x2, x2, -24          # stack frame
+    sw   x1, 20(x2)
+    sw   x10, 16(x2)
+    sw   x12, 12(x2)
+    sw   x13, 8(x2)
+
+    blt  x12, x13, do_split
+    j    end_mergesort
+
+do_split:
+    add  x14, x12, x13        # mid calc
+    srli x14, x14, 1          # divide by 2
+
+    # mergesort(left, mid)
+    addi x13, x14, 0
+    jal  x1, mergesort
+
+    # restore base and registers after return
+    lw   x10, 16(x2)
+    lw   x12, 12(x2)
+    lw   x13, 8(x2)
+
+    # mergesort(mid+1, right)
+    addi x12, x14, 1
+    jal  x1, mergesort
+
+    # restore
+    lw   x10, 16(x2)
+    lw   x12, 12(x2)
+    lw   x13, 8(x2)
+
+    # merge(left, mid, right)
+    addi x11, x14, 0
+    jal  x1, merge
+
+end_mergesort:
+    lw   x1, 20(x2)
+    addi x2, x2, 24
+    jalr x0, 0(x1)
+
+############################################################
+# merge(base=x10, left=x12, mid=x11, right=x13)
+# Merges array[left..mid] and array[mid+1..right]
+############################################################
+merge:
+    addi x2, x2, -48
+    sw   x1, 44(x2)
+    sw   x10, 40(x2)
+    sw   x11, 36(x2)
+    sw   x12, 32(x2)
+    sw   x13, 28(x2)
+
+    la   x14, temp          # temp array base
+    addi x15, x12, 0        # i = left
+    addi x16, x11, 1        # j = mid+1
+    addi x17, x12, 0        # k = left (for temp)
+
+merge_loop:
+    bgt  x15, x11, right_side   # if i > mid
+    bgt  x16, x13, left_side    # if j > right
+
+    # load A[i]
+    slli x18, x15, 2
+    add  x19, x10, x18
+    lw   x20, 0(x19)
+
+    # load A[j]
+    slli x21, x16, 2
+    add  x22, x10, x21
+    lw   x23, 0(x22)
+
+    blt  x20, x23, take_left
+    j    take_right
+
+take_left:
+    slli x24, x17, 2
+    add  x25, x14, x24
+    sw   x20, 0(x25)
+    addi x15, x15, 1
+    addi x17, x17, 1
+    j    merge_loop
+
+take_right:
+    slli x24, x17, 2
+    add  x25, x14, x24
+    sw   x23, 0(x25)
+    addi x16, x16, 1
+    addi x17, x17, 1
+    j    merge_loop
+
+left_side:                    # Copy remaining left side
+    bgt  x15, x11, copy_done
+    slli x18, x15, 2
+    add  x19, x10, x18
+    lw   x20, 0(x19)
+    slli x24, x17, 2
+    add  x25, x14, x24
+    sw   x20, 0(x25)
+    addi x15, x15, 1
+    addi x17, x17, 1
+    j    left_side
+
+right_side:                   # Copy remaining right side
+    bgt  x16, x13, copy_done
+    slli x21, x16, 2
+    add  x22, x10, x21
+    lw   x23, 0(x22)
+    slli x24, x17, 2
+    add  x25, x14, x24
+    sw   x23, 0(x25)
+    addi x16, x16, 1
+    addi x17, x17, 1
+    j    right_side
+
+copy_done:
+    # copy back from temp to array
+    addi x17, x12, 0          # k = left
+copyback_loop:
+    bgt  x17, x13, merge_exit
+    slli x24, x17, 2
+    add  x25, x14, x24
+    lw   x26, 0(x25)
+
+    add  x27, x10, x24
+    sw   x26, 0(x27)
+
+    addi x17, x17, 1
+    j    copyback_loop
+
+merge_exit:
+    lw   x1, 44(x2)
+    addi x2, x2, 48
+    jalr x0, 0(x1)
+
