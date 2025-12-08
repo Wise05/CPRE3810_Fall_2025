1256b990ab32a878abafe4f38f9d3b8d03cc7844
shanen <shanen@linuxvdi-f25-50.ece.iastate.edu>
2025-12-08 16:37:04 -0600

    Moved test files to the correct place.

--- proj2_sw/proj/riscv/Proj2_base_test.s
+++ proj2_sw/proj/riscv/Proj2_base_test.s
@@ -1,105 +1,162 @@
 .data
 var1:   .word 0x12345678
 var2:   .word 0xABCDEF01
-arr:	.word 1,2,3,4
+arr:    .word 1,2,3,4
 bytearr:.byte 0x12, 0x34, 0x56, 0x78
 halfarr:.half 0x1111, 0x2222
 
 .text
-
 main:
-
 ############################################################
 # LUI / AUIPC
 ############################################################
-	lui   x5, 0x12345      	
-	auipc x6, 0x10         	
-
+    lui   x5, 0x74565          # load upper immediate
+    auipc x6, 0x10             # PC-relative immediate add
 ############################################################
 # ADDI / ADD / SUB
 ############################################################
-	addi  x7, x0, 5        	
-	addi  x8, x0, 10        
-	sub   x10, x8, x7       
-	add   x9, x7, x8        
-
-############################################################
-# SLT / SLTI / SLTIU
-############################################################
-	addi  x17, x0, -1
-	slti  x19, x7, 10       
-	slt   x18, x7, x8       
-	sltiu x20, x17, 0xFF 	
-
-############################################################
-# SLL / SLLI / SRL / SRLI / SRA / SRAI
-############################################################
-	slli  x22, x7, 2        
-	srl   x23, x13, x7      
-	sll   x21, x7, x8       
-	srli  x24, x13, 4       
-	sra   x25, x13, x7      
-	srai  x26, x13, 4       
-
-############################################################
-# LOADS: LW, LB, LH, LBU, LHU
-############################################################
-	la	x27, var1
-	lb	x29, 3(x27)      	
-	lh	x30, 0(x27)      	
-	lbu   x31, 1(x27)      	
-	lhu   x1, 2(x27)        
-	lw	x28, 0(x27)      	
-
-############################################################
-# STORE: SW
-############################################################
-	sw	x28, 0(x27)      	
+    addi  x7, x0, 5
+    addi  x8, x0, 10
+    addi  x0, x0, 0
+    addi  x0, x0, 0
+    addi  x0, x0, 0
+    add   x9, x7, x8
+    addi  x0, x0, 0
+    addi  x0, x0, 0
+    addi  x0, x0, 0
+    sub   x10, x8, x7
 
 ############################################################
 # ANDI / AND / ORI / OR / XORI / XOR
 ############################################################
-	ori   x13, x0, 0xF   	
-	andi  x11, x9, 0x0F    	
-	and   x12, x11, x9     	
-	or	x14, x13, x12    	
-	xori  x15, x14, 0xAA 	
-	xor   x16, x15, x14    	
+    andi  x11, x9, 0x0F
+    addi  x0, x0, 0
+    addi  x0, x0, 0
+    addi  x0, x0, 0
+    and   x12, x11, x9
+    addi  x0, x0, 0
+    addi  x0, x0, 0
+    addi  x0, x0, 0
+    ori   x13, x0, 0xF
+    addi  x0, x0, 0
+    addi  x0, x0, 0
+    addi  x0, x0, 0
+    or    x14, x13, x12
+    addi  x0, x0, 0
+    addi  x0, x0, 0
+    addi  x0, x0, 0
+    xori  x15, x14, 0xAA
+    addi  x0, x0, 0
+    addi  x0, x0, 0
+    addi  x0, x0, 0
+    xor   x16, x15, x14
+    addi  x0, x0, 0
+    addi  x0, x0, 0
+    addi  x0, x0, 0
+############################################################
+# SLT / SLTI / SLTIU
+############################################################
+    addi  x17, x0, -1
+    slt   x18, x7, x8
+    slti  x19, x7, 10
+    addi  x0, x0, 0
+    sltiu x20, x17, 0xFF
+############################################################
+# SLL / SLLI / SRL / SRLI / SRA / SRAI
+############################################################
+    sll   x21, x7, x8
+    slli  x22, x7, 2
+    srl   x23, x13, x7
+    srli  x24, x13, 4
+    sra   x25, x13, x7
+    srai  x26, x13, 4
+############################################################
+# LOADS: LW, LB, LH, LBU, LHU
+############################################################
+    # la x27, var1  -->  expanded
+    lui   x27, %hi(var1)
+    addi  x0, x0, 0          # NOP
+    addi  x0, x0, 0          # NOP
+    addi  x0, x0, 0          # NOP
+    addi  x27, x27, %lo(var1)
 
+    addi  x0, x0, 0
+    addi  x0, x0, 0
+    addi  x0, x0, 0
+    lw    x28, 0(x27)
+    lb    x29, 3(x27)
+    lh    x30, 0(x27)
+    lbu   x31, 1(x27)
+    lhu   x1, 2(x27)
+############################################################
+# STORE: SW
+############################################################
+    sw    x28, 0(x27)
 ############################################################
 # BRANCHES: BEQ, BNE, BLT, BGE, BLTU, BGEU
 ############################################################
-	addi  x2, x0, 5
-	addi  x3, x0, 5
-	addi  x4, x0, 10
-
-	bne   x2, x4, notequal_label 
-	beq   x2, x3, equal_label   
+    addi  x2, x0, 5
+    addi  x3, x0, 5
+    addi  x4, x0, 10
+    addi  x0, x0, 0
+    addi  x0, x0, 0
+beq_test:
+    beq   x2, x3, equal_label
+    addi  x0, x0, 0
+    addi  x0, x0, 0
+    addi  x0, x0, 0
+    addi  x5, x0, 1
 equal_label:
-	blt   x2, x4, less_label 	
+    bne   x2, x4, notequal_label
+    addi  x0, x0, 0
+    addi  x0, x0, 0
+    addi  x0, x0, 0
+    addi  x6, x0, 2
 notequal_label:
-	bge   x4, x2, greater_label  
+    blt   x2, x4, less_label
+    addi  x0, x0, 0
+    addi  x0, x0, 0
+    addi  x0, x0, 0
+    addi  x7, x0, 3
 less_label:
-	bltu  x2, x4, unsigned_less_label  
+    bge   x4, x2, greater_label
+    addi  x0, x0, 0
+    addi  x0, x0, 0
+    addi  x0, x0, 0
+    addi  x8, x0, 4
 greater_label:
-	bgeu  x4, x2, unsigned_ge_label	
+    bltu  x2, x4, unsigned_less_label
+    addi  x0, x0, 0
+    addi  x0, x0, 0
+    addi  x0, x0, 0
+    addi  x9, x0, 5
 unsigned_less_label:
+    bgeu  x4, x2, unsigned_ge_label
+    addi  x0, x0, 0
+    addi  x0, x0, 0
+    addi  x0, x0, 0
+    addi  x10, x0, 6
 unsigned_ge_label:
-
 ############################################################
 # JAL / JALR
 ############################################################
-	jal   x11, jump_label   	
+    jal   x11, jump_label
+    addi  x0, x0, 0
+    addi  x0, x0, 0
+    addi  x0, x0, 0
+    jal   x0, end
+    addi  x0, x0, 0
+    addi  x0, x0, 0
+    addi  x0, x0, 0
+    addi  x12, x0, 9
 jump_label:
-	jalr  x0, 4(x11)        	
-
-############################################################
-# Final: WFI (halts processor)
-############################################################
-	wfi
+    jalr  x0, 0(x11)
+    addi  x0, x0, 0
+    addi  x0, x0, 0
+    addi  x0, x0, 0
+end:
+    wfi
 ############################################################
 # End of test
 ############################################################
-
-
 
